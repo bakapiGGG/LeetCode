0d6c9ab59119d57a24d3016bc45e1a0e87e01b6b
zyachin <129049634+bakapiGGG@users.noreply.github.com>
2023-08-27 21:54:39 +0800

    Time: 5348 ms (9.75%), Space: 20.3 MB (86.95%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,53 +1,22 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         
-        # res = []
-        res = set()
-        temp = []
-        sorted_nums = sorted(nums)
-
-        for pos, num in enumerate(sorted_nums):
-
-            sorted_arr = sorted_nums[:]
-            sorted_arr.pop(pos)
-
-            i = 0
-            j = len(sorted_arr)-1
-
-            while i < j:
-
-                if sorted_arr[j] + sorted_arr[i] > -num:
-                    j -= 1
-
-                elif sorted_arr[j] + sorted_arr[i] < -num:
-                    i += 1
-
-                else:
-                    
-                    if num >= sorted_arr[j]:
-
-                        # temp = [sorted_arr[i], sorted_arr[j], num]
-                        temp = (sorted_arr[i], sorted_arr[j], num)
-
-                    elif num <= sorted_arr[i]:
-
-                        temp = (num, sorted_arr[i], sorted_arr[j])
-                        # temp = [sorted_arr[i], sorted_arr[j], num]
-
-                    else:
-
-                        temp = (sorted_arr[i], num, sorted_arr[j])
-                        # temp = [sorted_arr[i], sorted_arr[j], num]
-                        
-                        
-                    res.add(temp)
-              
-                        
-                    i += 1
-                    
-                    
-                    temp = []
-                    
-                    
-        return res
+        ans = []
+        sortedNums = sorted(nums)
         
+        for i in range(len(nums)):
+            source = sortedNums[i]
+            j = i + 1
+            k = len(nums) - 1
+            while j < k:
+                potentialSum = source + sortedNums[j] + sortedNums[k]
+                if potentialSum > 0:
+                    k -= 1
+                elif potentialSum < 0:
+                    j += 1
+                else:
+                    if [source, sortedNums[j], sortedNums[k]] not in ans:
+                        ans.append([source, sortedNums[j], sortedNums[k]])
+                    k -= 1
+                    j += 1
+        return ans
